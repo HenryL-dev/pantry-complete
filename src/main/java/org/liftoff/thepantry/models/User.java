a618a8c18ac1a21f9651e8e5acd0661e71f3ce5b
Henry L <henry.lewisg59@gmail.com>
2022-09-30 00:17:17 -0500

    almost completed- fixing drop downs too close

--- src/main/java/org/liftoff/thepantry/models/User.java
+++ src/main/java/org/liftoff/thepantry/models/User.java
@@ -3,6 +3,7 @@ package org.liftoff.thepantry.models;
 
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Entity
 public class User extends AbstractEntity {
@@ -13,19 +14,23 @@ public class User extends AbstractEntity {
     @NotNull
     private String pwHash;
 
+
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
     public User() {}
 
     public User(String username, String password) {
         this.username = username;
-        this.pwHash = password;
+        this.pwHash = encoder.encode(password);
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
     }
 
 
