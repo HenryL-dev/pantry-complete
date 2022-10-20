0ee5aeae364cea0f5993a84326b269ebf8899ae1
Henry L <henry.lewisg59@gmail.com>
2022-10-19 22:47:22 -0500

    save from week

--- src/main/java/org/liftoff/thepantry/AuthenticationFilter.java
+++ src/main/java/org/liftoff/thepantry/AuthenticationFilter.java
@@ -21,7 +21,7 @@ public class AuthenticationFilter extends HandlerInterceptorAdapter {
     @Autowired
     AuthenticationController authenticationController;
 
-    private static final List<String> whitelist = Arrays.asList("index", "home", "about", "features","search/index", "/list/index", "/recipe", "/home", "/index", "/login", "/register", "/logout", "/css");
+    private static final List<String> whitelist = Arrays.asList("fragments", "index", "home", "about", "features","search/index", "/list/index", "/recipe", "/home", "/index", "/login", "/register", "/logout", "/css");
 
     private static boolean isWhitelisted(String path) {
         for (String pathRoot : whitelist) {
